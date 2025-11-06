905edbb24f69f3358acfcfc5e11304cc37937efc
Nakatina <qwafel1122@gmail.com>
2025-11-06 14:35:42 +0100

    Updated Meia to Medie
    Added the simple super class structure.

--- SP3 opgave/src/Media.java
+++
@@ -1,6 +0,0 @@
-public abstract class Media {
-    //TODO: Opret Felter
-    //TODO: Lav konstruktør
-    //TODO: Tilføj getters
-    //TODO: Lav metode play()
-}
---
+++ SP3 opgave/src/Medie.java
@@ -0,0 +1,21 @@
+public abstract class Medie {
+    //TODO: Opret Felter
+    //TODO: Lav konstruktør
+    //TODO: Tilføj getters
+    //TODO: Lav metode play()
+    String title;
+    int releaseYear;
+    double rating;
+    String category;
+
+    public Medie(String title, int releaseYear, double rating, String category) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+        this.category = category;
+    }
+
+    public abstract void play(); // Play method to be overridden
+}
+
+
