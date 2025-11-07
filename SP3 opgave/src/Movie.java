70c0b61dae983b49d7dbc9988a00d2fcad19eaa5
KristinaBengtsson <cph-kd104@stud.ek.dk>
2025-11-07 12:59:17 +0100

    film.txt
    Læs fra film.txt i Movie.java

--- SP3 opgave/src/Movie.java
+++ SP3 opgave/src/Movie.java
@@ -1,6 +1,9 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Movie extends Media {
     int duration; //filmens længde
-
+    //TODO indlæser fra txt/csv Filer.
     //Konstructor!
     public Movie(String title, int releaseYear, double rating, String category, int duration){
         super(title, releaseYear, rating, category);
@@ -15,5 +18,25 @@ public class Movie extends Media {
     //eksemple
     //Movie titanic = new Movie("Titanic", 1997, 7.8, "Romance", 195);
 
-    //TODO indlæser fra txt/csv Filer.
+    //Læs film fra .txt
+    public static void readMoviesFromTxt(){
+        try {
+            File file = new File("data/film.txt"); //peger på txt filen
+
+            Scanner input = new Scanner(file); //læser teksten i file (som peger på txt)
+
+            while (input.hasNextLine()){ //så længe der er en ny linje bliver den læst og vist
+                String line = input.nextLine();
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Kunne ikke læse filen");
+        }
+    }
+
+    //Midlertidig test main
+    public static void main(String[] args){
+        readMoviesFromTxt();
+    }
+
 }
