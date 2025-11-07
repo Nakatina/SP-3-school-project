cef8edbaffae687a5ee9e4e7a515b19fe49a1a3d
KristinaBengtsson <cph-kd104@stud.ek.dk>
2025-11-07 11:56:59 +0100

    TODO lister tilføjet ud fra miniopgave

--- SP3 opgave/src/Main.java
+++ SP3 opgave/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         //TODO: Udskriv velkomsttest
         //TODO: Indlæs brugere og medier
         //TODO: Vis startmenu og håndter brugerindput
+        //TODO: Gennemgå håndtering af fejl
+        //TODO: SIDTE DEL AF KODNINGEN! Login gemmes i extern fil
+        //TODO: VALGTFRIT EXTRA: Grafisk brugerflade
         Movie titanic = new Movie("Titanic", 1997, 7.8, "Romance", 195);
         titanic.play();
 
--- SP3 opgave/src/StreamingService.java
+++ SP3 opgave/src/StreamingService.java
@@ -2,6 +2,6 @@ public class StreamingService {
     //TODO: Opret liste af brugere
     //TODO: Opret liste af medier
     //TODO: Implementer registrer og login
-    //TODO:Implemteter search title pg kategori
+    //TODO:Implemteter search title pg kategori + udskriv fejbesked hvis ikke findes
     //TODO: Print velkommen login / opret dig.
 }
