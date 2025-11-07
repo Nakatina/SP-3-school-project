1981c0e585fc2e4a6944f6c9b697ab8f1d46e47e
KristinaBengtsson <cph-kd104@stud.ek.dk>
2025-11-07 11:56:59 +0100

    TODO lister tilføjet ud fra miniopgave

--- SP3 opgave/src/Main.java
+++ SP3 opgave/src/Main.java
@@ -1,10 +1,16 @@
+
 public class Main {
 
     public static void main(String[] args){
-        //TODO: Udskriv velkomsttest TJEK
-        //TODO: Indlæs brugere og medier BRUGERE TJEK
-        //TODO: Vis startmenu og håndter brugerinput TJEK
-        ConsoleApp ca = new ConsoleApp();
-        ca.startProgram();
+        //TODO: Udskriv velkomsttest
+        //TODO: Indlæs brugere og medier
+        //TODO: Vis startmenu og håndter brugerindput
+        //TODO: Gennemgå håndtering af fejl
+        //TODO: SIDTE DEL AF KODNINGEN! Login gemmes i extern fil
+        //TODO: VALGTFRIT EXTRA: Grafisk brugerflade
+        Movie titanic = new Movie("Titanic", 1997, 7.8, "Romance", 195);
+        titanic.play();
+
     }
+
 }
